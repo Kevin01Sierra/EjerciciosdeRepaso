ab3d503cb0dc11d9fcff36abd64caa0e2be15f2b
Kevin01Sierra <kevinnsg1602@gmail.com>
2019-04-03 11:47:37 -0500

    Ejercicios Repaso

---
+++ src/ejerciciosr/EjercicioN10.java
@@ -0,0 +1,20 @@
+
+package ejerciciosr;
+
+import java.util.Scanner;
+
+public class EjercicioN10 {
+    public void opration(){
+        Double Km, t, cm, cs, v;
+        System.out.println(" *** Ejercicio de conversion *** \n");
+        Scanner Entrada = new Scanner(System.in);
+        System.out.println("Ingrese el valor de la distancia recorrida por el proyectil en Km");
+        Km = Entrada.nextDouble();
+        System.out.println("Ingrese el valor del tiempo en que demora el proywectil en Minutos");
+        t = Entrada.nextDouble();
+        cm = Km * 1000;
+        cs = t * 60;
+        v = cm / cs;
+        System.out.println("La Velocidad del proyectil es: " +v+"m/s \n");
+    }
+}
---
+++ src/ejerciciosr/EjerciciosR.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciosr;
+
+public class EjerciciosR {
+
+    public void operation(){
+        int x, y, sum, res, mul, div;
+        x = 10;
+        y = 2;
+        sum = x + y;
+        res = x - y;
+        mul = x * y;
+        div = x / y;
+        System.out.println("Ejercicio # 4: \n");
+        System.out.println("La suma de " +x+ " + " +y+ " = " +sum);
+        System.out.println("La resta de " +x+ " - " +y+ " = " +res);
+        System.out.println("La multiplicascion de " +x+ " * " +y+ " = " +mul);
+        System.out.println("La divicion de " +x+ " / " +y+ " = " +div+ "\n");
+    }    
+}
